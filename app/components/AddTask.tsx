4f059285f28164951b299e537bc6a7345546ece3
Alberto Prado <aprado1012@gmail.com>
2023-05-04 14:23:17 -0600

    Agregamos  NewTaskValue

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,17 +1,34 @@
 "use client";
-import { useState } from 'react'
+import { FormEventHandler, useState } from 'react'
 import { AiOutlinePlus} from 'react-icons/ai'
 import Modal from './Modal'
 
 
 const AddTask = () => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTaskvalue, setNewTaskVale] = useState<string>('')
+
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(newTaskvalue)
+  }
   return (
     <div>
         <button onClick={() => setModalOpen(true)} className='btn btn-primary w-full'> Add new task
         <AiOutlinePlus  className='ml-2' size={18} /></button>
 
-        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+            <form onSubmit={handleSubmitNewTodo}>
+              <h3 className='font-bold text-lg'>Add new task</h3>
+               <div className="modal-action">
+               <input
+               value={newTaskvalue}
+               onChange={(e) => setNewTaskVale(e.target.value)}
+                type="text" placeholder="Type here" className="input input-bordered w-full w-full" />
+               <button type='submit' className='brtn'>Submit</button>
+               </div>
+            </form>
+        </Modal>
     </div>
     
   )
--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -2,15 +2,17 @@
 interface ModalProps {
     modalOpen: boolean,
     setModalOpen: (open:boolean) => boolean | void ;
-}
-const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen }) => {
+    children: React.ReactNode
+  }
+const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen, children }) => {
   return (
     <div className={`modal ${modalOpen ?  "modal-open" : ""}`}>
         <div className="modal-box relative">
             <label onClick={() => setModalOpen (false)}
-            className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-                <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+            className="btn btn-sm btn-circle absolute right-2 top-2">
+              ✕
+              </label>
+              {children}
         </div>
     </div>
   )
